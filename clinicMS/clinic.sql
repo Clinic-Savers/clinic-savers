a01ff66daa644f653d762b6b2ffa51af1338a9cd
Your Name <jann.chia.2020@scis.smu.edu.sg>
2022-03-23 16:16:48 +0800

    Updated clinic postal code to varchar

--- clinicMS/clinic.sql
+++ clinicMS/clinic.sql
@@ -32,7 +32,7 @@ DROP TABLE IF EXISTS `clinic`;
 CREATE TABLE IF NOT EXISTS `clinic` (
   `clinicName` varchar(128) NOT NULL,
   `clinicAddress` varchar(128) NOT NULL,
-  `clinicPostalCode` int(6) NOT NULL,
+  `clinicPostalCode` varchar(6) NOT NULL,
   `description` varchar(128) NOT NULL,
   PRIMARY KEY (`clinicName`,`clinicPostalCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -42,10 +42,10 @@ CREATE TABLE IF NOT EXISTS `clinic` (
 --
 
 INSERT INTO `clinic` (`clinicName`,`clinicAddress`,`clinicPostalCode`, `description`) VALUES
-('Raffles Medical', '52 North Bridge Rd, #02 - 17', 179103, 'Our team of family physicians provides care for patients of all ages. Medical care includes treatment of acute and chronic conditions for adults and children, vaccinations, legal services and health screening.'),
-('OneCare Medical Clinic Tiong Bahru', '11A Boon Tiong Rd, #01-08', 161011, 'OneCare Medical Clinics provide timely, affordable and professional healthcare. Our friendly team of general practitioners, family physicians, and nurses are always ready to help.'),
-('Sunnyvale Clinic & Surgery', '498 Jurong West Street 41', 640498, 'nfmaslnasl'),
-('Dayspring Medical Clinic (Tampines)', '2 Tampines Central 5, #04-09 Century Square', 529509, 'Minmed clinic is a CHAS registered General Practitioner (GP) clinic. We provide both teleconsult and in-clinic medical services such as doctor consultation and prescription, vaccinations and managing of chronic illnesses.')
+('Raffles Medical', '52 North Bridge Rd, #02 - 17', '179103', 'Our team of family physicians provides care for patients of all ages. Medical care includes treatment of acute and chronic conditions for adults and children, vaccinations, legal services and health screening.'),
+('OneCare Medical Clinic Tiong Bahru', '11A Boon Tiong Rd, #01-08', '161011', 'OneCare Medical Clinics provide timely, affordable and professional healthcare. Our friendly team of general practitioners, family physicians, and nurses are always ready to help.'),
+('Sunnyvale Clinic & Surgery', '498 Jurong West Street 41', '640498', 'nfmaslnasl'),
+('Dayspring Medical Clinic (Tampines)', '2 Tampines Central 5, #04-09 Century Square', '529509', 'Minmed clinic is a CHAS registered General Practitioner (GP) clinic. We provide both teleconsult and in-clinic medical services such as doctor consultation and prescription, vaccinations and managing of chronic illnesses.')
 ;
 COMMIT;
 
