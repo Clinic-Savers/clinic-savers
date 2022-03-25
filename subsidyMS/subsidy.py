4b05c4037aa7c34a288dd3fc554e824ee4e01acb
Your Name <jann.chia.2020@scis.smu.edu.sg>
2022-03-26 05:01:17 +0800

    Set dbURL environment variable

--- subsidyMS/subsidy.py
+++ subsidyMS/subsidy.py
@@ -5,11 +5,7 @@ from datetime import date
 from os import environ
 
 app = Flask(__name__)
-<<<<<<< Updated upstream
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/subsidy'
-=======
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
->>>>>>> Stashed changes
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
