1827590d8892e159d9ed9924d553fedc8457615e
Your Name <jann.chia.2020@scis.smu.edu.sg>
2022-03-31 18:52:40 +0800

    Added more clinics to match Singpass persona

--- clinicMS/clinic.sql
+++ clinicMS/clinic.sql
@@ -48,7 +48,10 @@ INSERT INTO `clinic` (`id`,`name`,`address`, `postalCode`, `email`) VALUES
 (3, 'Raffles Medical Anson Centre', '51 Anson Road, #01-51 Anson Centre', '079904', 'bryan.shing.2020@scis.smu.edu.sg'), 
 (4, 'Raffles Medical Bishan', 'Blk 283 Bishan Street 22, #01-177', '570283', 'bryan.shing.2020@scis.smu.edu.sg'), 
 (5, 'Raffles Medical Compass One', '1 Sengkang Square, #04-09, Compass One', '545078', 'bryan.shing.2020@scis.smu.edu.sg'), 
-(6, 'Raffles Medical Rivervale Mall', '11, Rivervale Crescent, #02-17 Rivervale Mall', '545082', 'bryan.shing.2020@scis.smu.edu.sg')
+(6, 'Raffles Medical Rivervale Mall', '11, Rivervale Crescent, #02-17 Rivervale Mall', '545082', 'bryan.shing.2020@scis.smu.edu.sg'), 
+(7, 'Raffles Medical Toa Payoh', 'Blk 177 Toa Payoh Central, #01-170', '310177', 'bryan.shing.2020@scis.smu.edu.sg'), 
+(8, 'Raffles Medical Hillion Mall', '17 Petir Road, Hillion Mall, #02-07', '678278', 'bryan.shing.2020@scis.smu.edu.sg')
+(9, 'Raffles Medical Tampines 1', '10 Tampines Central 1, #03-28', '529536', 'bryan.shing.2020@scis.smu.edu.sg')
 ;
 COMMIT;
 
